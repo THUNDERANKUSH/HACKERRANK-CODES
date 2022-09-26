c961d0ffb7ae322f23e9977b5f71b73e9def9716
ANKUSH KUMAR <114045890+THUNDERANKUSH@users.noreply.github.com>
2022-09-26 10:21:24 +0530

    Add files via upload

---
+++ M_302_The_MaxMin_Card_Game.py
@@ -0,0 +1,23 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+items = []
+for i in range(int(input())):
+    a=input()
+    if a[0:3] == 'Add':
+        x,b=a.split()
+        b = int(b)
+        items.append(b)
+    elif a[0:6] == 'Remove':
+        if(len(items) == 0):
+            print('Invalid')
+        else:
+            items.pop()
+    elif a[0:7] == 'CallMin':
+        if(len(items) == 0):
+            print('Invalid')
+        else:
+            print(min(items))
+    elif a[0:7] == 'CallMax':
+        if(len(items) == 0):
+            print('Invalid')
+        else:
+            print(max(items))
