f898dbdc81b680f0b31f8e6191451e1d95dd0554
ANKUSH KUMAR <114045890+THUNDERANKUSH@users.noreply.github.com>
2022-11-17 15:09:10 +0530

    Add files via upload

---
+++ Q_103_Trees_Postorder_Traversal.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+struct node {
+    
+    int data;
+    struct node *left;
+    struct node *right;
+  
+};
+
+struct node* insert( struct node* root, int data ) {
+        
+    if(root == NULL) {
+    
+        struct node* node = (struct node*)malloc(sizeof(struct node));
+
+        node->data = data;
+
+        node->left = NULL;
+        node->right = NULL;
+        return node;
+      
+    } else {
+      
+        struct node* cur;
+        
+        if(data <= root->data) {
+            cur = insert(root->left, data);
+            root->left = cur;
+        } else {
+            cur = insert(root->right, data);
+            root->right = cur;
+        }
+    
+        return root;
+    }
+}
+
+void postOrder( struct node *root) {
+    if (root != NULL) {
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main() {
+  
+    struct node* root = NULL;
+    
+    int t;
+    int data;
+
+    scanf("%d", &t);
+
+    while(t-- > 0) {
+        scanf("%d", &data);
+        root = insert(root, data);
+    }
+  
+    postOrder(root);
+    return 0;
+}
