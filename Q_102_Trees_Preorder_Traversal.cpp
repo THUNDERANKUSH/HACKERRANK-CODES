48d389cccf2e9d779a0ba9c2242be01a135325b3
ANKUSH KUMAR <114045890+THUNDERANKUSH@users.noreply.github.com>
2022-11-17 15:06:56 +0530

    Add files via upload

---
+++ Q_102_Trees_Preorder_Traversal.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+struct node {
+    
+    int data;
+    struct node *left;
+    struct node *right;
+  
+};
+
+struct node* insert( struct node* root, int data ) {
+        
+    if(root == NULL) {
+    
+        struct node* node = (struct node*)malloc(sizeof(struct node));
+
+        node->data = data;
+
+        node->left = NULL;
+        node->right = NULL;
+        return node;
+      
+    } else {
+      
+        struct node* cur;
+        
+        if(data <= root->data) {
+            cur = insert(root->left, data);
+            root->left = cur;
+        } else {
+            cur = insert(root->right, data);
+            root->right = cur;
+        }
+    
+        return root;
+    }
+}
+
+
+void preOrder( struct node *root) {
+if (root != NULL) {
+        printf("%d ", root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+
+int main() {
+  
+    struct node* root = NULL;
+    
+    int t;
+    int data;
+
+    scanf("%d", &t);
+
+    while(t-- > 0) {
+        scanf("%d", &data);
+        root = insert(root, data);
+    }
+  
+    preOrder(root);
+    return 0;
+}
