a392a35888eec35c53c9022535635874ef3d5347
ANKUSH KUMAR <114045890+THUNDERANKUSH@users.noreply.github.com>
2022-11-24 14:26:39 +0530

    Add files via upload

---
+++ Sherlock_and_Minimax.cpp
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <set>
+#include <map>
+#include <queue>
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <cctype>
+#include <cassert>
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define rer(i,l,u) for(int (i)=(int)(l);(i)<=(int)(u);++(i))
+#define reu(i,l,u) for(int (i)=(int)(l);(i)<(int)(u);++(i))
+#if defined(_MSC_VER) || __cplusplus > 199711L
+#define aut(r,v) auto r = (v)
+#else
+#define aut(r,v) typeof(v) r = (v)
+#endif
+#define each(it,o) for(aut(it, (o).begin()); it != (o).end(); ++ it)
+#define all(o) (o).begin(), (o).end()
+#define pb(x) push_back(x)
+#define mp(x,y) make_pair((x),(y))
+#define mset(m,v) memset(m,v,sizeof(m))
+#define INF 0x3f3f3f3f
+#define INFL 0x3f3f3f3f3f3f3f3fLL
+using namespace std;
+typedef vector<int> vi; typedef pair<int,int> pii; typedef vector<pair<int,int> > vpii;
+typedef long long ll; typedef vector<long long> vl; typedef pair<long long,long long> pll; typedef vector<pair<long long,long long> > vpll;
+typedef vector<string> vs; typedef long double ld;
+template<typename T, typename U> inline void amin(T &x, U y) { if(y < x) x = y; }
+template<typename T, typename U> inline void amax(T &x, U y) { if(x < y) x = y; }
+
+int A[102];
+int main() {
+	int N;
+	scanf("%d", &N);
+	rep(i, N) scanf("%d", &A[i]);
+	int P, Q;
+	scanf("%d%d", &P, &Q);
+	A[N] = P; A[N+1] = Q;
+	pair<int, int> ans = mp(-INF, INF);
+	rep(i, N+2) rep(j, i+1) rer(d, -1, 1) {
+		int m = (A[i] + A[j]) / 2 + d;
+		if(!(P <= m && m <= Q)) continue;
+		int x = INF;
+		rep(i, N)
+			amin(x, abs(A[i] - m));
+		amax(ans, mp(x, -m));
+	}
+	printf("%d\n", -ans.second);
+	return 0;
+}
